ae83e8fde1c91a6d199f015a8fb29123661fd0ec
Ray Wang <rw091402@gmail.com>
2025-12-21 14:15:24 +0800

    deleted redundant files

--- split.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-def split(main_games, split_ratio): 
-    split_index = int(len(main_games) * split_ratio)
-    test = main_games.iloc[:split_index]
-    train = main_games.iloc[split_index:]
-    return train, test
