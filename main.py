2bbfd7a1bbbc95d56908df4bb37b5ef6deb84624
Ray Wang <rw091402@gmail.com>
2025-12-23 12:23:04 +0800

    fixed modeling features

--- main.py
+++ main.py
@@ -14,8 +14,18 @@ games = pd.read_csv('nba_games/games.csv').sort_values('GAME_DATE_EST')
 #create a rolling average attr and split into train/test
 games = feature_funcs.rolling_avg(games, 'PTS', 5, 'PPG_L5')
 train, test = feature_funcs.split(games, .8)
-x_train, y_train = train[['FG3_PCT_home', 'AST_home', 'PPG_L5_home']], train['HOME_TEAM_WINS']
-x_test, y_test = test[['FG3_PCT_home', 'AST_home', 'PPG_L5_home']], test['HOME_TEAM_WINS']
+
+# Combine home and away features for training
+x_train = train[['PPG_L5_home', 'PPG_L5_away',
+                 'FG3_PCT_home', 'FG3_PCT_away',
+                 'AST_home', 'AST_away']]
+y_train = train['HOME_TEAM_WINS']
+
+# Combine home and away features for testing
+x_test = test[['PPG_L5_home', 'PPG_L5_away',
+               'FG3_PCT_home', 'FG3_PCT_away',
+               'AST_home', 'AST_away']]
+y_test = test['HOME_TEAM_WINS']
 
 #create model and feed train dataset/labels
 model = XGBClassifier()
@@ -24,4 +34,4 @@ model.fit(x_train, y_train)
 #create inferred labels and check accuracy
 y_pred = model.predict(x_test)
 accuracy = accuracy_score(y_pred, y_test)
-print('These features have an accuracy of: ' + str(accuracy))
+print('Model accuracy: ' + str(100 * accuracy) + '%')
